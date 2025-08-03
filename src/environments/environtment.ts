66a78074eb9e7ad90fa25fbd9b2ca737bfe73e28
jaehjoo <wnwoduq@naver.com>
2025-08-03 21:41:06 +0900

    add: void enviroment file

---
+++ src/environments/environtment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  userPoolId: '',
+  userPoolClientId: '',
+  identityPoolId: '',
+  region: 'ap-northeast-2',
+  googleClientId: '',
+  apiUrl: '',
+  cognitoDomain: '',
+  redirectSignIn: '',
+  redirectSignOut: ''
+};
